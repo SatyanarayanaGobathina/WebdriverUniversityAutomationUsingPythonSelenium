cf9634b8768445a55c37af73bd2aeedd7eed9f12
satya <93513456+SatyanarayanaGobathina@users.noreply.github.com>
2023-04-07 00:01:00 +0530

    this is second commit from satya in testing branch

--- ex1.py
+++ ex1.py
@@ -1,5 +1,5 @@
 print('hello world')
 print("this is second commmit from dev1, from aio unboxings ")
 print("creating new testing branch")
-print('this is first commit from satyanarayana from ubuntu in Testing branch')
+print("this is first commit from satyanarayana from ubuntu in Testing branch")
 
