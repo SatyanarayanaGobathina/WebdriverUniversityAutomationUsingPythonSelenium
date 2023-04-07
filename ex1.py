5cae54cf6b0867006679eeecb359303a3018e04b
satya <93513456+SatyanarayanaGobathina@users.noreply.github.com>
2023-04-07 12:20:18 +0530

    expecting merge conflict

--- ex1.py
+++ ex1.py
@@ -1,4 +1,4 @@
-print('hello world')
+
 print("this is second commmit from dev1, from aio unboxings ")
 print("creating new testing branch")
 print("this is first commit from satyanarayana from ubuntu in Testing branch")
